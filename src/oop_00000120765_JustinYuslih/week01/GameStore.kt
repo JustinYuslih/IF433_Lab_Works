59eb3572618573d69aa4f00da4d4894b3cce8c3b
Justin Y <133660225+SUPERSHOTT@users.noreply.github.com>
2026-02-06 16:41:35 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_00000120765_JustinYuslih/week01/GameStore.kt
+++ src/oop_00000120765_JustinYuslih/week01/GameStore.kt
@@ -1,18 +1,23 @@
 package oop_00000120765_JustinYuslih.week01
 
 fun main(){
-    val gameTitle : String = "GTA VI"
-    val price : Int = 1000000
+    val gameTitle : String? = "GTA VI"
+    val price : Int = 40000
 
-    val title = printReceipt(gameTitle = "GTA VI", price = 1000000)
+    val title = printReceipt(gameTitle = gameTitle, price = price)
 }
 
 fun calculateDiscount(price : Int): Int = if (price > 50000) price - 10000 else price - 5000
 
-fun printReceipt(gameTitle : String, price : Int){
+fun printReceipt(gameTitle : String?, price : Int){
     val disc = calculateDiscount(price)
 
+    var nameGame : String? = gameTitle
+
+    if (nameGame == null){
+        nameGame = "Tidak ada"
+    }
     println("--STRUK GAME--")
-    println("Name :  $gameTitle")
+    println("Name :  $nameGame")
     println("Price : $disc")
 }
