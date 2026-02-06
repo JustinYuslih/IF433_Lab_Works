f401f9c8d48c2121f8a2d33270b0098b29f7cd5d
Justin Y <133660225+SUPERSHOTT@users.noreply.github.com>
2026-02-06 16:17:13 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000120765_JustinYuslih/week01/CircleCalculator.kt
+++ src/oop_00000120765_JustinYuslih/week01/CircleCalculator.kt
@@ -1,15 +1,14 @@
 package oop_00000120765_JustinYuslih.week01
 
-fun main(args: Array<String>) {
-    // Variable definition
+fun main(){
     val radius: Double = 7.0
     val pi: Double = 3.14
-    // Calculation
+
     val area = pi * radius * radius
 
-    // Output Concatenation
     println("Radius: $radius, Area: $area")
-    // Logic check
+
     println(checkSize(area))
 }
+
 fun checkSize(area: Double): String = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
