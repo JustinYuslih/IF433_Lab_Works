443f1f451491a3766e3e8ee5aa0a5cfbcc779aa4
Justin Y <133660225+SUPERSHOTT@users.noreply.github.com>
2026-02-20 14:57:40 +0700

    week03: test all encapsulation logic in main

--- src/oop_00000120765_JustinYuslih/week03/Main.kt
+++ src/oop_00000120765_JustinYuslih/week03/Main.kt
@@ -2,6 +2,11 @@ package oop_00000120765_JustinYuslih.week03
 
 fun main(){
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000
+
+    e.salary = -1000
+    e.salary = 500000
+
+    e.increasePerformance()
+
+    println("Pajak yang harus dibayar : ${e.tax}")
 }
