07b7c322cd02f79abf8f47f6973a3ef26a00ca2d
Justin Y <133660225+SUPERSHOTT@users.noreply.github.com>
2026-03-01 12:58:06 +0700

    "week04: create Car class extending Vehicle"

---
+++ src/oop_00000120765_JustinYuslih/week04/Car.kt
@@ -0,0 +1,8 @@
+package oop_00000120765_JustinYuslih.week04
+
+// Car "Is-A" Vehicle. Parameter brand dilempar ke atas melalui Vehicle (brand)
+open class Car(brand: String, val numberOfDoors: Int): Vehicle (brand) {
+    fun openTrunk() {
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+    }
+}
