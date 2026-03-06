fc50684a3d2ac699ea8b8c6c114dcc7a697a9c44
Justin Y <133660225+SUPERSHOTT@users.noreply.github.com>
2026-03-06 14:43:40 +0700

    "week05: (task 1) test MathHelper overloading in main"

--- src/oop_00000120765_JustinYuslih/week05/Main.kt
+++ src/oop_00000120765_JustinYuslih/week05/Main.kt
@@ -3,8 +3,16 @@ package oop_00000120765_JustinYuslih.week05
 fun main(){
     val dosen1 = Dosen("Pak Alex", "0123456")
     val admin1 = Admin("Bu Siti")
+    val math = MathHelper()
 
     val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    val luas1 = math.hitungLuas(50)
+    println("Luas Pertama : ${luas1}")
+    val luas2 = math.hitungLuas(50,60)
+    println("Luas Kedua : ${luas2}")
+    val luas3 = math.hitungLuas(5.5)
+    println("Luas Ketiga : ${luas3}")
 
     println("=== AKTIVITAS PEGAWAI ===")
     for(pegawai in daftarPegawai){
