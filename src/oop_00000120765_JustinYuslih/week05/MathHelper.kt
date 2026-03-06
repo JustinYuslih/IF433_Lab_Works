29e7645bfab1219a01717a7cdb8146286a848984
Justin Y <133660225+SUPERSHOTT@users.noreply.github.com>
2026-03-06 14:37:32 +0700

    "week05: (task 1) create MathHelper class with overloading"

---
+++ src/oop_00000120765_JustinYuslih/week05/MathHelper.kt
@@ -0,0 +1,14 @@
+package oop_00000120765_JustinYuslih.week05
+
+class MathHelper {
+    fun hitungLuas(){}
+    fun hitungLuas(sisi: Int): Int{
+        return sisi * sisi
+    }
+    fun hitungLuas(panjang: Int, lebar: Int): Int{
+        return panjang * lebar
+    }
+    fun hitungLuas(jariJari: Double): Double{
+        return 3.14 * jariJari * jariJari
+    }
+}
