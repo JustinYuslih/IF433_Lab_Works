5768d7cb6136edda28af7ececd9351df25750d6c
Justin Y <133660225+SUPERSHOTT@users.noreply.github.com>
2026-03-13 15:29:15 +0700

    "week06: (task) instantiate smart devices in main"

--- src/oop_00000120765_JustinYuslih/week06/Main.kt
+++ src/oop_00000120765_JustinYuslih/week06/Main.kt
@@ -15,6 +15,10 @@ fun main(){
     val pay1 = Gopay()
     val pay2 = CreditCard()
 
+    val smartLamp = SmartLamp("001", "Ruang Tamu")
+    val smartSpeaker = SmartSpeaker("002", "Google Nest Dapur")
+    val smartCCTV = SmartCCTV("003", "Ezviz Garasi")
+
     println("\n=== TESTING CHECKOUT ====")
     processCheckout(pay1, 50000.0)
     processCheckout(pay2, 150000.0)
