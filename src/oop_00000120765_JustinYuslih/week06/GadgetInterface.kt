3720a7283c38dc5967a9c36cb18ba159e6f9c13b
Justin Y <133660225+SUPERSHOTT@users.noreply.github.com>
2026-03-13 14:51:00 +0700

    "week06: create Camera and Phone interfaces with default methods"

---
+++ src/oop_00000120765_JustinYuslih/week06/GadgetInterface.kt
@@ -0,0 +1,13 @@
+package oop_00000120765_JustinYuslih.week06
+
+interface camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+interface Phone{
+    fun turnOn(){
+        println("Sinyal seluler mencari jaringan.")
+    }
+
+}
