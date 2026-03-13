32afb049da314eb033815e240f6bb3c55d2574be
Justin Y <133660225+SUPERSHOTT@users.noreply.github.com>
2026-03-13 14:55:51 +0700

    "week06: create Gopay and CreditCard"

--- src/oop_00000120765_JustinYuslih/week06/PaymentMethod.kt
+++ src/oop_00000120765_JustinYuslih/week06/PaymentMethod.kt
@@ -3,3 +3,15 @@ package oop_00000120765_JustinYuslih.week06
 interface PaymentMethod{
     fun pay(amount: Double)
 }
+
+class Gopay: PaymentMethod{
+    override fun pay(amount: Double){
+        println("Processing Rp$amount via gopay server")
+    }
+}
+
+class CreditCard: PaymentMethod{
+    override fun pay(amount: Double){
+        println("Contacting bank for Rp$amount")
+    }
+}
