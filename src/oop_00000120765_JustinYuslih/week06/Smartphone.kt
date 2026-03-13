391d0a92b3c26fc19e38b8e9b865ca6906d703e1
Justin Y <133660225+SUPERSHOTT@users.noreply.github.com>
2026-03-13 14:54:09 +0700

    "week06: create PaymentMethod contract"

--- src/oop_00000120765_JustinYuslih/week06/Smartphone.kt
+++ src/oop_00000120765_JustinYuslih/week06/Smartphone.kt
@@ -3,7 +3,7 @@ package oop_00000120765_JustinYuslih.week06
 class Smartphone : camera, Phone{
     override fun turnOn(){
         super<camera>.turnOn()
-        super<phone>.turnOn()
+        super<Phone>.turnOn()
         println("Sistem operasi smartphone berhasil booting.")
     }
 }
