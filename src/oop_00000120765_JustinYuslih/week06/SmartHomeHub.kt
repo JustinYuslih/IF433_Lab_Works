1d335537d2ff403be13027e41a1c76fd293d8773
Justin Y <133660225+SUPERSHOTT@users.noreply.github.com>
2026-03-13 15:25:47 +0700

    "week06: (task) create SmartHomeHub logic"

---
+++ src/oop_00000120765_JustinYuslih/week06/SmartHomeHub.kt
@@ -0,0 +1,18 @@
+package oop_00000120765_JustinYuslih.week06
+
+class SmartHomeHub{
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice){
+        devices.add(device)
+        println("Menambahkan $device ke list")
+    }
+
+    fun turnOffAllSwitches(){
+        for( device in devices){
+            when(device){
+                is Switchable -> device.turnOff()
+            }
+        }
+    }
+}
