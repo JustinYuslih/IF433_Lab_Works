1c3db5f5f54fdf305968c1c4d72ddefb3d3b3caf
Justin Y <133660225+SUPERSHOTT@users.noreply.github.com>
2026-03-27 15:09:58 +0700

    "week07: (trial) demonstrate regular class equality failure"

---
+++ src/oop_00000120765_JustinYuslih/week07/Main.kt
@@ -0,0 +1,17 @@
+package oop_00000120765_JustinYuslih.week07
+
+fun main(){
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient()
+    client.connect()
+
+    println("=== TEST REGULAR CLASS ===")
+    val reg1 = RegularUser("Alice", 22)
+    val reg2 = RegularUser("Alice", 22)
+    println(reg1)
+    println("Sama? ${reg1 == reg2}")
+}
---
+++ src/oop_00000120765_JustinYuslih/week07/UserModels.kt
@@ -0,0 +1,3 @@
+package oop_00000120765_JustinYuslih.week07
+
+class RegularUser(val name: String, val age: Int)
