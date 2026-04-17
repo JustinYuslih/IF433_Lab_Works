1201e2b0d9ba906ceae899af210562f2ae466838
Justin Y <justin.yuslih@student.umn.ac.id>
2026-04-17 15:28:44 +0700

    "week08: (task) create sealed class Product and data classes"

---
+++ src/oop_00000120765_JustinYuslih/week08/ECommerceModels.kt
@@ -0,0 +1,17 @@
+package oop_00000120765_JustinYuslih.week08
+
+import java.net.IDN
+
+sealed class Product{
+    data class Electronic(
+        val id: String,
+        val name: String,
+        val warrantyMonths: Int
+    ): Product()
+
+    data class Clothing(
+        val id: String,
+        val name: String,
+        val size: String
+    ): Product()
+}
