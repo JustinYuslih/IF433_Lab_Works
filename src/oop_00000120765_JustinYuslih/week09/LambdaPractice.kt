c2680806fab85c643b364d5be994d34759826f7c
Justin Y <justin.yuslih@student.umn.ac.id>
2026-04-24 14:57:55 +0700

    "week09: create basic lambdas using explicit and implicit syntax"

---
+++ src/oop_00000120765_JustinYuslih/week09/LambdaPractice.kt
@@ -0,0 +1,10 @@
+package oop_00000120765_JustinYuslih.week09
+
+fun main(){
+    println("=== TEST LAMBDA ===")
+    val sumLambda = {a: Int, b: Int -> a+b}
+    println("Hasil sum: ${sumLambda(5,10)}")
+
+    val squareImplicit: (Int) -> Int = {it * it}
+    println("Hasil square = ${squareImplicit(4)}")
+}
