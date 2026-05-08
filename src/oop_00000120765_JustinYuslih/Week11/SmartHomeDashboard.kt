ff93168ec6d7a6ab12ede00fb1ffa46a5c643d29
Justin Y <justin.yuslih@student.umn.ac.id>
2026-05-08 15:34:31 +0700

    "week11: (task) calculate total power usage "

--- src/oop_00000120765_JustinYuslih/Week11/SmartHomeDashboard.kt
+++ src/oop_00000120765_JustinYuslih/Week11/SmartHomeDashboard.kt
@@ -34,4 +34,9 @@ fun main(){
         println("--- Rangkuman Perangkat Pintar ---")
         println("Total Perangkat Terdaftar: ${this.size}")
     }
+
+    val totalPower = homeDevices.run{
+        sumOf{it.powerLoad}
+    }
+    println("Total Daya: $totalPower")
 }
