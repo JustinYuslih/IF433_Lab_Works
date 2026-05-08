8bb55c70f7ddca5091cc55bde334df971a62ffc5
Justin Y <justin.yuslih@student.umn.ac.id>
2026-05-08 14:49:58 +0700

    "week10: (task) populate coin repository"

--- src/oop_00000120765_JustinYuslih/week10/CryptoDashboard.kt
+++ src/oop_00000120765_JustinYuslih/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000120765_JustinYuslih.week10
 
 fun main(){
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 65000.0))
+    coinRepo.add(Coin("ETH", 3000.0))
+    coinRepo.add(Coin("USDT", 1.0))
 }
