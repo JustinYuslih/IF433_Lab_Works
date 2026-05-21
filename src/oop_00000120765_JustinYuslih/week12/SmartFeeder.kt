d5c54637ad2a996ebd2b59b63f0357054ce84593
Justin Y <justin.yuslih@student.umn.ac.id>
2026-05-21 19:12:12 +0700

    "week12: (task) implement hardware validation with throw"

--- src/oop_00000120765_JustinYuslih/week12/SmartFeeder.kt
+++ src/oop_00000120765_JustinYuslih/week12/SmartFeeder.kt
@@ -2,5 +2,10 @@ package oop_00000120765_JustinYuslih.week12
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+
     return 0
 }
