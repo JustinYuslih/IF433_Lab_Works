215fc0bf0ea4fd826f6ced0135a17ead9578c85e
Justin Y <justin.yuslih@student.umn.ac.id>
2026-05-21 19:13:33 +0700

    "week12: (task) handle feeding execution with multiple catch"

--- src/oop_00000120765_JustinYuslih/week12/SmartFeeder.kt
+++ src/oop_00000120765_JustinYuslih/week12/SmartFeeder.kt
@@ -17,4 +17,14 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+
+    try {
+        dispenseKibble(requestedGram = 80, availableGram = currentKibbleStock, isJammed = false)
+    } catch (e: DispenserJamException) {
+        println("Caught Error: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Caught Error: ${e.message}")
+    } catch (e: Exception) {
+        println("Caught General Error: Terjadi kesalahan tidak terduga.")
+    }
 }
