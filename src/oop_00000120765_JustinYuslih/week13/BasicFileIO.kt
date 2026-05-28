6054b741b602626c2ab7c482248f66969fccbc49
Justin Y <justin.yuslih@student.umn.ac.id>
2026-05-28 18:57:39 +0700

    "week13: implement readText and readLines"

--- src/oop_00000120765_JustinYuslih/week13/BasicFileIO.kt
+++ src/oop_00000120765_JustinYuslih/week13/BasicFileIO.kt
@@ -8,5 +8,14 @@ fun print(){
     println("File berhasil dibuat dan ditulis.")
     file.appendText("Line 2: Menambahkan konfigurasi baru.\n")
     println("Text berhasil di-append")
+    println("\n=== TEST READ TEXT ===")
+    val fullContent = file.readText()
+    println("Membaca sekaligus:\n$fullContent")
+
+    println("=== TEST READ LINE ===")
+    val lines = file.readLines()
+    lines.forEachIndexed { index, string ->
+        println("Isi index $index: $lines")
+    }
 }
 
